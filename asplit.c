1c8445e87364ec9205d1ebbebb05e5a58e52a679
Deven Blake <blakedevendesu@gmail.com>
2021-02-16 21:31:17 -0500

    manpage, delete python version, improvements

--- asplit.c
+++ asplit.c
@@ -2,29 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define LINEM 1024 // line max length
+#define LINEM 1024
 
-/*
- * asplit written by deven blake; to the extent possible this program
- * is licensed under the public domain where applicable and the
- * UNLICENSE where not applicable
- * 
- * plan9ports' and FreeBSD's cat implementations, util-linux's colrm,
- * and gnu's ls referenced heavily. especially colrm.
- * 
- * i find this utility to be of use, but my programming is not the
- * best. if you feel you can write this better, be my guest. i will
- * probably use your implementation :>
-*/
+/* returns bool, if true the line var is shorter than the actual input */
+#define OFLOW(line) ((strlen(line) == LINEM-1) && (line[LINEM-1] != '\n'))
+
+/* asplit written by deven blake 2021; licensed under the public domain
+ * where applicable and the UNLICENSE where not applicable */
 
 void
 usage(char *argv0) {
 	printf("Usage: %s [STRING] [SELECTION]\n", argv0);
 	printf("\
-Splits ASCII lines from STDIN by STRING and outputs the span of text between\n\
-the SELECTION-1th and SELECTIONth occurrence of STRING in each line of text.\n\
-If there is no specified SELECTION, outputs the amount of times STRING\n\
-occurs in each line of text.\n\
+    Splits ASCII lines from STDIN by STRING and outputs the span of text \n\
+    between the SELECTION-1th and SELECTIONth occurrence of STRING in each \n\
+    line of text. If there is no specified SELECTION, outputs the amount of\n\
+    times STRING occurs in each line of text.\n\
 ");
 }
 
@@ -32,22 +25,29 @@ int
 main(int argc, char *argv[]) {
 	FILE *in;
 	char line[LINEM];
-	int lineindex = 0; // 1-indexed
-	int lp; // points to a place in a Line
-	char pc; // last-Printed character
-	int sp; // points to a place in an arg String
+	int lineindex = 0; /* line numbers start at 1 */
+	int lp;
+	char pc;
+	int sp;
 	int occurrences = 0;
 	FILE *out;
+	int overflow = 0;
 
 	if(argc < 2) { usage(argv[0]); return 0; }
 
-	in = stdin; // could add [FILE] to the args but it doesn't seem useful
+	in = stdin;
 	out = stdout;
 
 	while(fgets(line, LINEM, in)) {
+		if(overflow && OFLOW(line) == 0) {
+			overflow = 0;
+			continue;
+		} else if(overflow)
+			continue;
 		++lineindex;
 		pc = 0;
-		if((strlen(line) == LINEM-1) && (line[LINEM-1] != '\n'))
+		overflow = OFLOW(line);
+		if(overflow)
 			fprintf(
 				stderr,
 				"%s:%s:%d: Maximum line length possibly exceeded\n",
@@ -55,13 +55,15 @@ main(int argc, char *argv[]) {
 			);
 		occurrences = 0;
 		for(lp = 0; lp < strlen(line); lp++) {
-			for(sp = 0; (line[lp+sp] == *(argv[1]+sp)) && line[lp+sp] && *(argv[1]+sp); sp++);
+			/* checks to see if the string at the point == argv[1]
+			 * if (sp == strlen(argv[1])) after the loop, it does */
+			for(sp = 0; (line[lp+sp] == *(argv[1]+sp)) && (line[lp+sp] != '\0') && (*(argv[1]+sp) != '\0'); sp++);
 			if(sp == strlen(argv[1])) {
 				occurrences += 1;
 				lp += sp - 1;
 			} else if(argc > 2 && occurrences == atoi(argv[2])) {
 				pc = line[lp];
-				fprintf(out, "%c", line[lp]);
+				fprintf(out, "%c", pc);
 			}
 		}
 		if(argc < 3) fprintf(out, "%d\n", occurrences);
